16221ac14a778964b16023885d4bf1b67080ae00
DevGenix <you@example.com>
2021-03-05 09:09:17 -0500

    Base game

---
+++ lobby.py
@@ -0,0 +1,26 @@
+import random
+import sys
+import time
+
+players = ["Red", "Blue", "Green", "Yellow", "Orange", "Black", "White", "Purple"]
+
+def set_lobby():
+    imposter = random.choice(players)
+    print("CREWMATE \nThere is 1 imposter amoung us"  )
+    time.sleep(2)
+    player_input = raw_input("Guess the imposter: ")
+
+    if player_input.lower() == imposter:
+        print("{}".format(player_input) + " was the imposter")
+        print("VICTORY")
+    else:
+        print("{}".format(player_input) + " was not the imposter")
+        player_input = raw_input("Guess the imposter: ")
+        if player_input.lower() == imposter:
+            print("{}".format(player_input) + " was the imposter")
+            print("VICTORY")
+        else:
+            print("{}".format(player_input) + " was not the imposter")
+            print("DEFEAT")
+            sys.exit()
+set_lobby()
